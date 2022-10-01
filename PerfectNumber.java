0864a5f002a83b55164bf64ef3c42cdfff260b32
shivamsharma8 <113785642+shivamsharma8@users.noreply.github.com>
2022-10-01 11:05:27 +0530

    Add files via upload

---
+++ PerfectNumber.java
@@ -0,0 +1,22 @@
+public class PerfectNumber {
+
+    public static boolean isPerfectNumber(int number ){
+
+        if ( number >0){
+            int sum = 0;
+
+            for (int i =1 ; i< number; i++){
+
+                if ( number % i ==0){
+
+                    sum+=i;
+                }
+            }
+
+            if (sum==number){
+                return true;
+            }
+        }
+        return false;
+    }
+}
